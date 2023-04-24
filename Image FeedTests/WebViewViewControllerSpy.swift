2fad16b37928678e055537505e31c3885b7d617d
Ubskiy <ArsenAvakov1@yandex.ru>
2023-04-24 22:36:50 +0300

    Коммит доп файлов

---
+++ Image FeedTests/WebViewViewControllerSpy.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewViewControllerSpy.swift
+//  Image FeedTests
+//
+//  Created by Арсений Убский on 24.04.2023.
+//
+
+import ImageFeed
+import Foundation
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+
+    var loadRequestCalled: Bool = false
+
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+
+    func setProgressValue(_ newValue: Float) {
+
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+
+    }
+}
